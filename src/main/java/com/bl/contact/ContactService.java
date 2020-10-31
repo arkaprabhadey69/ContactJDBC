3050f51ce00afbeeb0088e59b29cb3cc95128297
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-31 15:05:33 +0530

    [Arka]Added:Service to add contacts to contacts table and subsequently other tables having dependency

--- src/main/java/com/bl/contact/ContactService.java
+++ src/main/java/com/bl/contact/ContactService.java
@@ -13,33 +13,42 @@ public class ContactService {
         contactDBService= ContactDBService.getInstance();
         this.contactDataList=new ArrayList<>();
     }
+    //Service To Retrieve All Data
     public List<Contact> readContactData() throws SQLException {
         this.contactDataList= contactDBService.readData();
         return this.contactDataList;
 
     }
+    //Service to Retrieve Contacts within a given Date Range
     public List<Contact> readContactsBetweenDateRange(LocalDate start, LocalDate end){
         this.contactDataList=contactDBService.readDateRangeData(start,end);
         return this.contactDataList;
 
     }
+    //Service To get contacts based on city
     public List<Contact> readContactsByCity(String city){
         this.contactDataList=contactDBService.readContactsByCity(city);
         return this.contactDataList;
 
     }
+    //Service To get contacts based on state
     public List<Contact> readContactsByState(String city){
         this.contactDataList=contactDBService.readContactsByState(city);
         return this.contactDataList;
 
     }
+    //Service To update address based on name
     public void updateContactsAddress(String name, String address){
         int result=contactDBService.updateContactData(name,address);
         if(result==0) return;
         Contact contact=this.getContactData(name);
         if(contact!=null) contact.address= address;
     }
-
+    //Service To add contact to addressbook
+    public void addContactToAddressBook(String firstName, String lastName, String address, String city, String state, String zip, String number, String email, LocalDate start) throws SQLException {
+        contactDataList.add(contactDBService.addContactToAddress( firstName, lastName, address,city, state,zip,number,email, start));
+    }
+    //Service To get contact to contactList by name
     public Contact getContactData(String name) {
         return this.contactDataList.stream()
                 .filter(employeePayrollData -> employeePayrollData.firstName.equals(name))
@@ -47,6 +56,7 @@ public class ContactService {
                 .orElse(null);
 
     }
+    //Service To check if DB is in sync
     public boolean checkIfDataBaseIsInSync(String name) throws SQLException {
         List<Contact> employeePayrollDataList= contactDBService.getContactFromDB(name);
         return employeePayrollDataList.get(0).equals(getContactData(name));
