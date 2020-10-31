78ff53c49587ed352eb53591b41a50e6e9703c6a
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-31 14:13:12 +0530

    [Arka]Added:Services to update address and check if DB is in sync with local system

--- src/main/java/com/bl/contact/ContactService.java
+++ src/main/java/com/bl/contact/ContactService.java
@@ -23,4 +23,34 @@ public class ContactService {
         return this.contactDataList;
 
     }
+    public List<Contact> readContactsByCity(String city){
+        this.contactDataList=contactDBService.readContactsByCity(city);
+        return this.contactDataList;
+
+    }
+    public List<Contact> readContactsByState(String city){
+        this.contactDataList=contactDBService.readContactsByState(city);
+        return this.contactDataList;
+
+    }
+    public void updateContactsAddress(String name, String address){
+        int result=contactDBService.updateContactData(name,address);
+        if(result==0) return;
+        Contact contact=this.getContactData(name);
+        if(contact!=null) contact.address= address;
+    }
+
+    public Contact getContactData(String name) {
+        return this.contactDataList.stream()
+                .filter(employeePayrollData -> employeePayrollData.firstName.equals(name))
+                .findFirst()
+                .orElse(null);
+
+    }
+    public boolean checkIfDataBaseIsInSync(String name) throws SQLException {
+        List<Contact> employeePayrollDataList= contactDBService.getContactFromDB(name);
+        return employeePayrollDataList.get(0).equals(getContactData(name));
+
+    }
+
 }
