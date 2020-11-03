d6f0bd3193a29cde975dff92297bf0162634081f
arkaprabhadey <euphoricdey420@gmail.com>
2020-11-03 12:01:33 +0530

    [Arka]Added:Test to add multiple contacts

--- src/test/java/com/bl/contact/ContactServiceTest.java
+++ src/test/java/com/bl/contact/ContactServiceTest.java
@@ -4,7 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -15,7 +18,7 @@ public class ContactServiceTest {
     public void readData() throws SQLException {
         ContactService contactService = new ContactService();
         List<Contact> contactList = contactService.readContactData();
-        Assert.assertEquals(3, contactList.size());
+        Assert.assertEquals(4, contactList.size());
 
     }
     @Test
@@ -23,7 +26,7 @@ public class ContactServiceTest {
         ContactService contactService = new ContactService();
         LocalDate start=LocalDate.of(2018,4,4);
         List<Contact> contactList = contactService.readContactsBetweenDateRange(start,LocalDate.now());
-        Assert.assertEquals(3, contactList.size());
+        Assert.assertEquals(4, contactList.size());
 
     }
     @Test
@@ -55,6 +58,22 @@ public class ContactServiceTest {
        contactService.addContactToAddressBook("Sayak","Mondal","Hazra","Delhi","NCR", "700056","986754534","abc@yahoo.com",LocalDate.now());
        boolean result = contactService.checkIfDataBaseIsInSync("Sayak");
         Assert.assertTrue(result);
+
+    }
+    @Test
+    public void givenMultipleNamesShouldBeAdded() throws SQLException {
+        ContactService contactService= new ContactService();
+        contactService.readContactData();
+        Contact c= new Contact(0,"Saunak","Mondal","Hazra","Delhi","NCR", "700056","986754534","abc@yahoo.com",LocalDate.now());
+        Contact c2= new Contact(0,"Krish","Mondal","Hazra","Delhi","NCR", "700056","986754534","abc@yahoo.com",LocalDate.now());
+        List<Contact> c3= new ArrayList<>();
+        c3.add(c);
+        c3.add(c2);
+        Instant start=Instant.now();
+        contactService.addMultipleEmployee(c3);
+        Instant end=Instant.now();
+        System.out.println("Time: "+ Duration.between(start,end));
+        Assert.assertEquals(6,contactService.countEntries());
 
     }
 }
